c31aacb8ee3c4fd5ce0069750558c65fe9cebcac
abdi <abdifatah952@gmail.com>
2024-10-28 20:48:01 -0500

    "added auth"

---
+++ GroceryApp/Models/ErrorWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorWrapper.swift
+//  GroceryApp
+//
+//  Created by abdifatah ahmed on 10/28/24.
+//
+
+import Foundation
---
+++ GroceryApp/Views/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  GroceryApp
+//
+//  Created by abdifatah ahmed on 10/28/24.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
